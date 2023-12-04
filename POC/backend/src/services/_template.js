c9825c1517186d32cd038b01ee817fcc4a34d9b0
Mazettt <martin.d-herouville@epitech.eu>
2023-12-04 22:42:40 +0100

    (+) template service and base github service

---
+++ POC/backend/src/services/_template.js
@@ -0,0 +1,43 @@
+import { Router } from 'express';
+import axios from 'axios';
+
+export const id = '';
+export const name = '';
+export const description = '';
+export const color = '#000000';
+export const icon = '/serviceIcon.svg';
+
+export const triggers = {
+    1: {
+        name: '',
+        description: '',
+        fields: [
+            {
+                name: '',
+                description: '',
+                type: 'text'
+            }
+        ],
+        check: async (userData, params, token) => {
+            return null;
+        }
+    }
+};
+
+export const reactions = {
+    1: {
+        name: '',
+        description: '',
+        fields: [
+            {
+                name: '',
+                description: '',
+                type: 'text'
+            }
+        ],
+        execute: async (userData, params, token, triggerData) => {
+        }
+    }
+};
+
+export const router = Router();
---
+++ POC/backend/src/services/github.js
@@ -0,0 +1,134 @@
+import { Router } from 'express';
+import axios from 'axios';
+
+export const router = Router();
+
+// router.get('/oauth', async (req, res) => {
+//     res.status(200).json({ msg: 'TODO' });
+// });
+
+// router.get('/oauth/callback', async (req, res) => {
+//     res.status(200).json({ msg: 'TODO' });
+// });
+
+export const id = 'github';
+export const name = 'Github';
+export const description = 'Github service';
+export const color = '#6e5494';
+export const icon = '/github.svg';
+
+export const triggers = {
+    1: {
+        name: 'New commit',
+        description: 'Triggers when a new commit is pushed to a repository',
+        fields: [
+            {
+                name: 'Repository',
+                description: 'The repository to watch',
+                type: 'text'
+            }
+        ],
+        check: async (userData, params, token) => {
+            console.log(`${name} trigger 1 check`);
+            console.log('userData:', userData);
+            console.log('params:', params);
+            console.log('token:', token);
+            return null;
+        }
+    },
+    2: {
+        name: 'New issue',
+        description: 'Triggers when a new issue is created',
+        fields: [
+            {
+                name: 'Repository',
+                description: 'The repository to watch',
+                type: 'text'
+            }
+        ],
+        check: async (userData, params, token) => {
+            console.log(`${name} trigger 2 check`);
+            console.log('userData:', userData);
+            console.log('params:', params);
+            console.log('token:', token);
+            return null;
+        }
+    },
+    3: {
+        name: 'New pull request',
+        description: 'Triggers when a new pull request is created',
+        fields: [
+            {
+                name: 'Repository',
+                description: 'The repository to watch',
+                type: 'text'
+            }
+        ],
+        check: async (userData, params, token) => {
+            console.log(`${name} trigger 3 check`);
+            console.log('userData:', userData);
+            console.log('params:', params);
+            console.log('token:', token);
+            return null;
+        }
+    }
+};
+
+export const reactions = {
+    1: {
+        name: 'Create issue',
+        description: 'Creates a new issue',
+        fields: [
+            {
+                name: 'Repository',
+                description: 'The repository to create the issue in',
+                type: 'text'
+            },
+            {
+                name: 'Title',
+                description: 'The title of the issue',
+                type: 'text'
+            },
+            {
+                name: 'Body',
+                description: 'The body of the issue',
+                type: 'text'
+            }
+        ],
+        execute: async (userData, params, token, triggerData) => {
+            console.log(`${name} action 1 execute`);
+            console.log('userData:', userData);
+            console.log('params:', params);
+            console.log('token:', token);
+            console.log('triggerData:', triggerData);
+        }
+    },
+    2: {
+        name: 'Create pull request',
+        description: 'Creates a new pull request',
+        fields: [
+            {
+                name: 'Repository',
+                description: 'The repository to create the pull request in',
+                type: 'text'
+            },
+            {
+                name: 'Title',
+                description: 'The title of the pull request',
+                type: 'text'
+            },
+            {
+                name: 'Body',
+                description: 'The body of the pull request',
+                type: 'text'
+            }
+        ],
+        execute: async (userData, params, token, triggerData) => {
+            console.log(`${name} action 2 execute`);
+            console.log('userData:', userData);
+            console.log('params:', params);
+            console.log('token:', token);
+            console.log('triggerData:', triggerData);
+        }
+    }
+};
