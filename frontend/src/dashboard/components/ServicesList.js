1f2a8618afca4b084dacff20a98ff3d51f11709c
unknown <leo.baldachino@epitech.eu>
2023-12-11 17:08:48 +0100

    [+] Disabled scrolling [+] Creating services (existing) list view

---
+++ frontend/src/dashboard/components/ServicesList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Grid from '@mui/material/Grid';
+
+export default function ServicesDash() {
+    return (
+        <Grid container direction="row" spacing={5} style={{overflow: 'hidden'}}>
+        </Grid>
+    );
+}
