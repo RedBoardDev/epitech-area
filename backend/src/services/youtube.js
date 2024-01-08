62b317a2c9d64e7ee162715316ab77e96c01bad0
LucasB9 <lucas.binder@epitech.eu>
2024-01-08 14:08:17 +0100

    (refactor)[/] add the refresh_token for the google authenticate

--- backend/src/services/youtube.js
+++ backend/src/services/youtube.js
@@ -8,6 +8,33 @@ export const description = 'Youtube service';
 export const color = '#ff0000';
 export const icon = '/youtube.png';
 
+async function refreshToken(userId, refreshToken) {
+    console.log('Refreshing token...');
+    const { youtubeClientId, youtubeClientSecret } = process.env;
+    try {
+        const response = await axios.post('https://oauth2.googleapis.com/token', {
+            client_id: youtubeClientId,
+            client_secret: youtubeClientSecret,
+            refresh_token: refreshToken,
+            // redirect_uri: `http://localhost:6500/fr/service/oauth/${id}/callback`,
+            grant_type: 'refresh_token',
+        }, {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Accept': 'application/json',
+            },
+        })
+        const newToken = JSON.stringify({ access_token: response?.data?.access_token || undefined, refresh_token: response?.data?.refresh_token || refreshToken, });
+        console.log(newToken);
+        await db.updateServiceOauth(userId, id, newToken);
+        console.log('Token refreshed, trying next time.');
+        return newToken;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
+
 export const connect = async (userId) => {
     const { youtubeClientId, youtubeClientSecret } = process.env;
 
@@ -22,6 +49,7 @@ export const connect = async (userId) => {
             response_type: 'code',
             scope: scopes.join(' '),
             state: userId,
+            access_type: 'offline',
         };
 
         const query = Object.keys(params).map((key) => `${key}=${encodeURIComponent(params[key])}`).join('&');
@@ -58,7 +86,7 @@ export const callback = async (code) => {
             </html>
         `;
 
-        return { status: "success", action: htmlResponse, token: response?.data?.access_token || undefined };
+        return { status: "success", action: htmlResponse, token: JSON.stringify({ access_token: response?.data?.access_token || undefined, refresh_token: response?.data?.refresh_token || undefined, }) };
     } catch (error) {
         console.error('Error fetching access token:', error);
 
@@ -74,11 +102,16 @@ export const triggers = [
         fields: [
         ],
         check: async (autoId, userData, params, checkData, token) => {
-            try {
+            const { access_token, refresh_token } = JSON.parse(token);
+            try {   
+                console.log("token: ",token);
+                console.log("access_token: ", access_token);
+                console.log("refresh_token: ", refresh_token);
+
                 console.log(`${name} trigger 1 checking...`);
                 const resp = await axios.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails&myRating=like&maxResults=1`, {
                     headers: {
-                        Authorization: `Bearer ${token}`,
+                        Authorization: `Bearer ${access_token}`,
                         Accept: '*/*',
                         'Content-Type': 'application/json',
                     }
@@ -87,6 +120,8 @@ export const triggers = [
                 if (!likedVideo)
                     return null;
                 const lastLikedVideo = likedVideo.id;
+                console.log(lastLikedVideo);
+                console.log(checkData.lastLikedVideo);
                 if (checkData.lastLikedVideo && lastLikedVideo === checkData.lastLikedVideo)
                     return null;
                 db.updateAutomation(userData.id, autoId, `trigger_check_data = '${JSON.stringify({ lastLikedVideo: lastLikedVideo })}'`);
@@ -95,7 +130,10 @@ export const triggers = [
                     data: lastLikedVideo
                 };
             } catch (error) {
-                console.error(error);
+                if (error.response.status === 401)
+                    await refreshToken(userData.id, refresh_token);
+                else
+                    console.error(error);
                 return null;
             }
         }
@@ -122,6 +160,7 @@ export const reactions = [
             },
         ],
         execute: async (userData, params, token, triggerData) => {
+            const { access_token, refresh_token } = JSON.parse(token);
             try {
                 const options = {
                     snippet: {
@@ -135,7 +174,7 @@ export const reactions = [
                 };
                 const headers = {
                     headers: {
-                        "Authorization": `Bearer ${token}`,
+                        "Authorization": `Bearer ${access_token}`,
                         "Accept": 'application/json',
                         "Content-Type": "application/json",
                     }
