ffa919fdf84eb55dc765b31da01f15e1b40a90fc
unknown <leo.baldachino@epitech.eu>
2024-01-08 14:00:14 +0100

    [+] About.json test & completed automations tests

--- backend/tests/automations.test.js
+++ backend/tests/automations.test.js
@@ -23,3 +23,32 @@ test("Get all automations", async () => {
     expect(response.statusCode).toBe(200);
 });
 
+test("Get all automations - bad token", async () => {
+    const response = await request("https://area.mazettt.fr/api/en")
+        .get("/automations")
+        .set('Authorization', 'Bearer ' + loginToken + "bad");
+    expect(response.statusCode).toBe(403);
+});
+
+test("Get automation by id", async () => {
+    const response = await request("https://area.mazettt.fr/api/en")
+        .get("/automations/" + 1)
+        .set('Authorization', 'Bearer ' + loginToken);
+    expect(response.statusCode).toBe(200);
+});
+
+test("Add automation", async () => {
+    const response = await request("https://area.mazettt.fr/api/en")
+        .post("/automations")
+        .set('Authorization', 'Bearer ' + loginToken)
+        .send({
+            trigger_service_id: "string",
+            trigger_id: 0,
+            trigger_params: "{}",
+            reaction_service_id: "string",
+            reaction_id: 0,
+            reaction_params: "{}"
+        });
+    expect(response.statusCode).toBe(201);
+});
+
